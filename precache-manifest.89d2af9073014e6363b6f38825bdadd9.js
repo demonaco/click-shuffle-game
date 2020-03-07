429e89a9d76a982cbc67a1eb71cbd5b5388468e3
Matt DeMonaco <mdemonaco@gmail.com>
2020-03-06 19:31:16 -0800

    Updates

---
+++ precache-manifest.89d2af9073014e6363b6f38825bdadd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da858afa3456419a3fa56890a3b5e314",
+    "url": "/click-shuffle-game/index.html"
+  },
+  {
+    "revision": "4e9a1911f3f867802ca9",
+    "url": "/click-shuffle-game/static/css/main.7c4c5ed2.chunk.css"
+  },
+  {
+    "revision": "37387625b431c88405d8",
+    "url": "/click-shuffle-game/static/js/2.de521c22.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/click-shuffle-game/static/js/2.de521c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9a1911f3f867802ca9",
+    "url": "/click-shuffle-game/static/js/main.4c5100a7.chunk.js"
+  },
+  {
+    "revision": "c153add0297309aa85d6",
+    "url": "/click-shuffle-game/static/js/runtime-main.2158771e.js"
+  },
+  {
+    "revision": "f35c43bb585827ab6af9768c7235d7a3",
+    "url": "/click-shuffle-game/static/media/pikachu.f35c43bb.jpg"
+  }
+]);
